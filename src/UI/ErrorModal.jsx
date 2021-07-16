9cec1ede1cfaf47884760d6252ec9c0d909cee78
Glen Adamson <glen.adamson@terraquest.co.uk>
2021-07-16 08:58:39 +0100

    Working with React Portals

--- src/UI/ErrorModal.jsx
+++ src/UI/ErrorModal.jsx
@@ -2,21 +2,41 @@ import Card from "./Card";
 import Button from "./Button";
 import styles from "./ErrorModal.module.css";
 
+const Backdrop = ({ onClose }) => {
+  return <div onClick={onClose} className={styles.backdrop} />;
+};
+
+const ModalOverlay = ({ title, description, onClose }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{description}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ title, description, onClose }) => {
   return (
     <>
-      <div onClick={onClose} className={styles.backdrop} />
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{description}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onClose}>Okay</Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={onClose} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={title}
+          description={description}
+          onClose={onClose}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
